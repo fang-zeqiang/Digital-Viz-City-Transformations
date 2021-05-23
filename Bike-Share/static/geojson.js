7bfd18996f67ff0a0557792cfeffb2d5adfc3ea5
fang-zeqiang <zeqiang.fang@foxmail.com>
2021-05-23 16:36:21 +0800

    update the bike share

--- Bike-Share/static/geojson.js
+++ Bike-Share/static/geojson.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:284d07284a23cc0c60d605d4804e025c38c6c059dbecdb3bd5be636b0832b9aa
-size 224342793
+oid sha256:33ddee3c7ec0686e357b59231bdb8df1ba8599c0e4f611e65400633bff62e358
+size 26596385
